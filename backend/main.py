e1e7171be31f294068b0bc70ad5d2340086cad27
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-13 00:22:22 +0300

    update contact route

--- backend/main.py
+++ backend/main.py
@@ -42,6 +42,26 @@ def create_contact():
     return jsonify({"message": "User created successfully"}), 201
 
 
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User Info updated sucessfully."}), 200
+
+
+
+
+
 
 
 
